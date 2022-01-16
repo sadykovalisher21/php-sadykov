548e19227c6f9973a4015c5ecb9afdcd4b2109eb
fadeevdan77 <97813283+sadykovalisher21@users.noreply.github.com>
2022-01-16 16:47:36 +0500

    asd

--- 5/gen_xls.php
+++ 5/gen_xls.php
@@ -1,6 +1,7 @@
 <?php
   session_start();
-  if(!$_SESSION["rule"]) {
+  if(!$_SESSION["rule"]) header("Location: .");
+
   header("Expires: Mon, 1 Apr 1974 05:00:00 GMT");
   header("Last-Modified: " . gmdate("D,d M YH:i:s") . " GMT");
   header("Cache-Control: no-cache, must-revalidate");
