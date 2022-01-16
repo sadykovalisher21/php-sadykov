63888607e64621cb2335e90e99983e2ef7f935fb
fadeevdan77 <97813283+sadykovalisher21@users.noreply.github.com>
2022-01-16 16:55:26 +0500

    asd

--- 5/delete_invest.php
+++ 5/delete_invest.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 if($_SESSION["rule"] == 2) {
  $conn = mysqli_connect("eu-cdbr-west-02.cleardb.net","bfb0d61ccbcfdd","76448f94", "heroku_f9c5d8662f884a1") or die ("Невозможно подключиться к серверу");
  $zapros="DELETE FROM invest WHERE id=" . $_GET['id'];
